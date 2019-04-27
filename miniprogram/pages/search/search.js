a3abfda7385367d72335c6f0cfce94a5f5d54e1c
Wang Xiaoding <xiaoding12345@126.com>
2019-04-27 15:48:50 +0800

    尝试数据库
    完成了云函数的部署，还没有写完wxml向js传输数据

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,8 +1,48 @@
+const app = getApp()
+
 Page({
     data: {
         inputShowed: false,
-        inputVal: ""
+        inputVal: "",
+        input_score:"",
+        counterId:"",
+        id:"",
+        add:null,
     },
+    add: function (e) {
+        var that = this;
+        const db = wx.cloud.database()      //建立引用
+        var id = e.target.dataset.id;
+        //console.log(content);
+        this.setData({
+            input_score: e.detail.value
+          })
+        db.collection('grade').add({     //使用collection
+          data: {
+            count: 1,
+            txt: input_score,
+          },
+
+          success: res => {
+            // 在返回结果中会包含新创建的记录的 _id
+            this.setData({
+              counterId: res._id,
+              count: 1
+            })
+            wx.showToast({
+              title: '新增记录成功',
+            })
+            console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id,input_score)
+          },
+          fail: err => {
+            wx.showToast({
+              icon: 'none',
+              title: '新增记录失败'
+            })
+            console.error('[数据库] [新增记录] 失败：', err)
+          }
+        })
+      },
     showInput: function () {
         this.setData({
             inputShowed: true
