3bd4c01485b9cc16b624549b5025aca163d94a51
Wang Xiaoding <xiaoding12345@126.com>
2019-04-28 14:38:36 +0800

    完成类数据库input-增
    使用e.detail.value，输入框输入时通过bindinput绑定的函数实时显示，点击按钮后获取变量的值上传至数据库

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,26 +1,67 @@
 const app = getApp()
-
+var test = ""
 Page({
     data: {
         inputShowed: false,
         inputVal: "",
-        input_score:"",
+        input_score:"123",
         counterId:"",
         id:"",
-        add:null,
+    },
+    input: function(e)
+    {
+        this.setData({
+          input_score: e.detail.value
+        })
+        test = e.detail.value;
+        console.log("显示一下：",test)
+    },
+    addtest:function(e){
+        
+      const db = wx.cloud.database()      //建立引用
+
+      console.log("显示一下：",test)
+
+
+      db.collection('grade').add({     //使用collection
+        data: {
+          count: 1,
+          txt: test,
+        },
+
+        success: res => {
+          // 在返回结果中会包含新创建的记录的 _id
+          this.setData({
+            counterId: res._id,
+            count: 1
+          })
+          wx.showToast({
+            title: '新增记录成功',
+          })
+          console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id,input_score)
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '新增记录失败'
+          })
+          console.error('[数据库] [新增记录] 失败：', err)
+        }
+      })
     },
     add: function (e) {
-        var that = this;
+        
         const db = wx.cloud.database()      //建立引用
         var id = e.target.dataset.id;
-        //console.log(content);
-        this.setData({
-            input_score: e.detail.value
-          })
+
+        test = e.detail.value;
+        console.log("显示一下：",test)
+
+
         db.collection('grade').add({     //使用collection
           data: {
             count: 1,
-            txt: input_score,
+            txt: test,
           },
 
           success: res => {
@@ -43,6 +84,7 @@ Page({
           }
         })
       },
+      
     showInput: function () {
         this.setData({
             inputShowed: true
